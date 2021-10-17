b637b3f60d9692fa28b90df1b27f70a9cfe6f33d
kingcaubalejo <lokokram22@gmail.com>
2021-10-17 13:39:15 +0800

    Sidebar details

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('navigation')
+  navigation!: ElementRef;
 
   hide: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
+    console.log("OnInit", this.navigation.nativeElement);
   }
+
+  ngAfterViewInit(): void {
+    console.log("AfterViewInit", this.navigation.nativeElement);
+  }
+
 
 }
